033ee54c09bdd436457418a1c6cc526b7b7763f4
Paloma Wink <paloma.wink@hotmail.com>
2023-08-21 14:24:40 -0300

    Requisito 5 corrigido

--- backend/src/middlewares/validationRegister.js
+++ backend/src/middlewares/validationRegister.js
@@ -1,10 +1,10 @@
 const validationNewProduct = (req, res, next) => {
   const { name } = req.body;
   if (!name) {
-    res.status(400).json({ message: '"name" is required' });
+    return res.status(400).json({ message: '"name" is required' });
   }
   if (name.length <= 5) {
-    res.status(422).json({ message: '"name" length must be at least 5 characters long' });
+    return res.status(422).json({ message: '"name" length must be at least 5 characters long' });
   }
   next();
 };
---
+++ backend/src/middlewares/validationRegisterSale.js
@@ -0,0 +1,5 @@
+/* const validateRegisterNewSale = (req, res, next) => {
+    const { itemsSold } = req.body;
+};
+
+module.exports = validateRegisterNewSale; */
